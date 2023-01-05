8b0a634b0bec871757c4f5dc9a054f1276d03409
Karl Haines <kmhnashville@gmail.com>
2023-01-05 16:59:37 -0600

    added which-key.nvim, revamped keymaps, also revamp alpha page

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ require('opts')     -- Options
 require('keys')     -- Keymaps
 require('plug-config')
 require('lsp-config')
-
 -- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
@@ -13,23 +12,30 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   group = packer_group,
   pattern = vim.fn.expand '$MYVIMRC',
 })
-
 --[[ dash config, alpha.nvim ]]
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
+dashboard.section.header.val = {
+[[    _   __                _          ]],
+[[   / | / /__  ____ _   __(_)___ ___  ]],
+[[  /  |/ / _ \/ __ \ | / / / __ `__ \ ]],
+[[ / /|  /  __/ /_/ / |/ / / / / / / / ]],
+[[/_/ |_/\___/\____/|___/_/_/ /_/ /_/  ]],
+}
 dashboard.section.buttons.val = {
-  dashboard.button( "t r", "  Recent Files" , "<cmd>Telescope frecency<CR>"),
-  dashboard.button( "n", "  New file" , "<cmd>ene<CR>"),
-  dashboard.button( "e", "  Explore NvimTree" , "<cmd>NvimTreeToggle<CR>"),
-  dashboard.button( "t f", "  Find Files" , "<cmd>Telescope find_files<CR>"),
-  dashboard.button( "t p", "  Projects" , "<cmd>Telescope project<CR>"),
-  dashboard.button( "c", "  NVIM Config" , "<cmd>e $HOME/.config/nvim/init.lua<CR>"),
-  dashboard.button( "s", "  Run PackerSync" , "<cmd>ene<bar>:PackerSync<CR>"),
-  dashboard.button( "q", "  Quit NVIM ", "<cmd>qa<CR>"),
+  dashboard.button( "r", "  Frequent/recent files", "<cmd>Telescope frecency<CR>"),
+  dashboard.button( "n", "  New file", "<cmd>ene<CR>"),
+  dashboard.button( "e", "  Explore NvimTree", "<cmd>NvimTreeToggle<CR>"),
+  dashboard.button( "f", "  Find files", "<cmd>Telescope find_files<CR>"),
+  dashboard.button( "c", "  Neovim config", "<cmd>e $HOME/.config/nvim/init.lua<CR>"),
+  dashboard.button("pl", "  Projects", "<cmd>Telescope project<CR>"),
+  dashboard.button( "s", "  Session list", "<cmd>Telescope possession list<CR>"),
+  dashboard.button("ps", "  Run PackerSync", "<cmd>ene<bar>:PackerSync<CR>"),
+  dashboard.button( "q", "  Quit Neovim", "<cmd>qa<CR>"),
 }
 dashboard.section.footer.val = {
   [[ ]],
-  [[    lean, mean, (all lua) neovim config by linuxkarl ]],
+  [[   Lean, mean, (100% LUA) Neovim config by linuxkarl   ]],
   [[ telegram: @linuxkarl or email: karlhaines615@yahoo.com ]],
 }
 require('neovide')
--- lua/keys.lua
+++ lua/keys.lua
@@ -8,28 +8,6 @@ vim.g.mapleader = ','
 options = {
   noremap = true
 }
-
-map('n', '<leader><esc>', '<cmd>nohlsearch<cr>', options)
-map('n', '<leader>r', ':source $MYVIMRC<cr>', options)
-map('n', '<leader>bn', '<cmd>bnext<cr>', options)
-map('n', '<leader>bp', '<cmd>bprev<cr>', options)
-map('n', '<leader>bp', ':badd ', options)
-map('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', options)
-map('n', '<leader>tt', '<cmd>terminal<cr>', options)
-map('n', '<leader>tc', '<cmd>Telescope colorscheme<cr>', options)
-map('n', '<leader>tg', '<cmd>Telescope live_grep<cr>', options)
-map('n', '<leader>tfr', '<cmd>Telescope frecency<cr>', options)
-map('n', '<leader>tff', '<cmd>Telescope find_files<cr>', options)
-map('n', '<leader>tfb', '<cmd>Telescope file_browser<cr>', options)
-map('n', '<leader>tb', '<cmd>Telescope buffers<cr>', options)
-map('n', '<leader>th', '<cmd>Telescope help_tags<cr>', options)
-map('n', '<leader>tP', '<cmd>Telescope project<cr>', options)
-map('n', '<leader>tp', '<cmd>Telescope possession list<cr>', options)
-map('n', '<leader>tr', '<cmd>Telescope oldfiles<cr>', options)
-map('n', '<leader>ts', '<cmd>Telescope symbols<cr>', options)
-map('n', '<leader>qq', '<cmd>qa!<cr>', options)
-map('n', '<leader>wq', '<cmd>wqa!<cr>', options)
-
 local wk = require("which-key")
 -- As an example, we will create the following mappings:
 --  * <leader>ff find files
@@ -41,14 +19,42 @@ local wk = require("which-key")
 -- and hide <leader>1
 
 wk.register({
+  e = { "<cmd>NvimTreeToggle<CR>", "Explore NvimTree" },
+  n = { "<cmd>e $MYVIMRC<cr>", "Neovim configuration" },
+  f = {
+    name = "File functions",
+    n = { "<cmd>ene<CR>", "New file" },
+    r = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
+    f = { "<cmd>Telescope frecency<cr>", "Frequently used files" },
+    z = { "<cmd>Telescope find_files<cr>", "Find files" },
+    b = { "<cmd>Telescope file_browser<cr>", "Folder browser" },
+  },
+  b = {
+    name = "Buffer functions",
+    l = { "<cmd>Telescope buffers<cr>", "Telescope buffer picker" },
+    a = { ":badd ", "Add new buffer" },
+    d = { "<cmd>bdelete<cr>", "Delete buffer" },
+    n = { "<cmd>bnext<cr>", "Next buffer" },
+    p = { "<cmd>bprev<cr>", "Previous buffer" },
+  },
+  p = {
+    name = "Packer functions",
+    p = { "<cmd>PackerStatus<cr>", "Run PackerStatus" },
+    s = { "<cmd>PackerSync<cr>", "Run PackerSync" },
+    i = { "<cmd>PackerInstall<cr>", "Run PackerInstall" },
+    c = { "<cmd>PackerClean<cr>", "Run PackerClean" },
+    x = { "<cmd>PackerCompile<cr>", "Run PackerCompile" },
+  },
   t = {
     name = "Telescope", -- optional group name
     c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme browser" },
+    f = "files",
+    fb = { "<cmd>Telescope file_browser<cr>", "File/folder browser" },
     ff = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-    fr = { "<cmd>Telescope frecency<cr>", "Frequently Used Files", noremap=false },
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false },
-    u = { "<cmd>Telescope symbols<cr>", "Symbol browser" },
-    p = { "<cmd>Telescope project<cr>", "Project browser" },
+    fr = { "<cmd>Telescope frecency<cr>", "Frequently used Files", noremap=false },
+    fo = { "<cmd>Telescope oldfiles<cr>", "Open recent File", noremap=false },
+    u = { "<cmd>Telescope symbols picker<cr>", "Symbol browser" },
+    p = { "<cmd>Telescope projects picker<cr>", "Project browser" },
     s = {
       name = "Sessions",
       c = { "<cmd>PosClose<cr>", "Session close" },
@@ -58,15 +64,8 @@ wk.register({
       m = { "<cmd>PosMigrate<cr>", "Session migrate" },
       s = { "<cmd>PosSave<cr>", "Session save" },
     },
-  },
-  f = {
-    name = "File actions",
-    n = { "<cmd>ene<CR>", "New file" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
-    f = { "<cmd>Telescope frecency", "Frequently used files" },
-    z = { "<cmd>Telescope find_files", "Find files" },
-    b = { "<cmd>Telescope file_browser", "Folder browser" },
+    v = { "<cmd>Telescope vim_options<cr>", "vim options" },
   },
   ["1"] = "which_key_ignore",  -- special label to hide it in the popup
-  b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
+  --b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
 }, { prefix = "<leader>" })
