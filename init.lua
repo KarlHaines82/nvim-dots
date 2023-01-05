734f14add294b2977383d545a2e26e9490634883
Karl Haines <kmhnashville@gmail.com>
2023-01-04 23:36:25 -0600

    Hacking away...

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
---[[ TODO:
- popup.nvim
- Keymaps and leader pages
- ]]
-
 --[[ Imports ]]
 require('plugins')  -- plugins
 require('vars')     -- Variables
@@ -14,7 +9,7 @@ require('lsp-config')
 -- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | silent! LspStop | silent! LspStart | PackerCompile',
+  command = 'source <afile> | silent! LspStop | silent! LspStart | PackerCompile | echo PackerCompile complete..',
   group = packer_group,
   pattern = vim.fn.expand '$MYVIMRC',
 })
@@ -23,29 +18,21 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
 dashboard.section.buttons.val = {
-  dashboard.button( "t r", "  Recent Files" , ":Telescope frecency<CR>"),
-  dashboard.button( "n", "  New file" , ":ene<CR>"),
-  dashboard.button( "e", "  Explore NvimTree" , ":NvimTreeToggle<CR>"),
-  dashboard.button( "t f", "  Find Files" , ":Telescope find_files<CR>"),
-  dashboard.button( "t p", "  Projects" , ":Telescope project<CR>"),
-  dashboard.button( "c", "  NVIM Config" , ":e $HOME/.config/nvim/init.lua<CR>"),
-  dashboard.button( "s", "  Run PackerSync" , ":ene<bar>:PackerSync<CR>"),
-  dashboard.button( "q", "  Quit NVIM ", ":qa<CR>"),
+  dashboard.button( "t r", "  Recent Files" , "<cmd>Telescope frecency<CR>"),
+  dashboard.button( "n", "  New file" , "<cmd>ene<CR>"),
+  dashboard.button( "e", "  Explore NvimTree" , "<cmd>NvimTreeToggle<CR>"),
+  dashboard.button( "t f", "  Find Files" , "<cmd>Telescope find_files<CR>"),
+  dashboard.button( "t p", "  Projects" , "<cmd>Telescope project<CR>"),
+  dashboard.button( "c", "  NVIM Config" , "<cmd>e $HOME/.config/nvim/init.lua<CR>"),
+  dashboard.button( "s", "  Run PackerSync" , "<cmd>ene<bar>:PackerSync<CR>"),
+  dashboard.button( "q", "  Quit NVIM ", "<cmd>qa<CR>"),
 }
 dashboard.section.footer.val = {
   [[ ]],
   [[    lean, mean, (all lua) neovim config by linuxkarl ]],
   [[ telegram: @linuxkarl or email: karlhaines615@yahoo.com ]],
 }
+require('neovide')
 dashboard.config.opts.noautocmd = true
-
-if vim.g.neovide == true then
-  vim.g.neovide_transparency = 0.8
-  vim.g.neovide_cursor_vfx_mode = 'railgun'
-  vim.g.neovide_cursor_vfx_particle_density = 15.0
-else
-  vim.cmd[[set background=]]
-end
-
 vim.cmd[[autocmd User AlphaReady echo 'ready']]
 alpha.setup(dashboard.opts)
--- lua/plug-config.lua
+++ lua/plug-config.lua
@@ -1,6 +1,6 @@
 require('nvim-tree').setup{
   view = {
-    width = 24,
+    width = 30,
   },
 }
 require('glow').setup{
@@ -11,27 +11,26 @@ require('glow').setup{
 }
 require('lualine').setup{}
 require('kanagawa').setup({
-    undercurl = true,           -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true},
-    statementStyle = { bold = true },
-    typeStyle = {},
-    variablebuiltinStyle = { italic = true},
-    specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords
-    transparent = true,        -- do not set background color
-    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
-    globalStatus = true,       -- adjust window separators highlight for laststatus=3
-    terminalColors = true,      -- define vim.g.terminal_color_{0,17}
-    colors = {},
-    overrides = {},
-    theme = "default"           -- Load "default" theme or the experimental "light" theme
+  undercurl = true,           -- enable undercurls
+  ommentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true},
+  statementStyle = { bold = true },
+  typeStyle = {},
+  variablebuiltinStyle = { italic = true},
+  specialReturn = true,       -- special highlight for the return keyword
+  specialException = true,    -- special highlight for exception handling keywords
+  transparent = true,        -- do not set background color
+  dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+  globalStatus = true,       -- adjust window separators highlight for laststatus=3
+  terminalColors = true,      -- define vim.g.terminal_color_{0,17}
+  colors = {},
+  overrides = {},
+  theme = "default"           -- Load "default" theme or the experimental "light" theme
 })
 
 require('dressing').setup({
   input = {
-    -- Set to false to disable the vim.ui.input implementation
     enabled = true,
     -- Default prompt string
     default_prompt = "Input:",
@@ -41,22 +40,16 @@ require('dressing').setup({
     start_in_insert = true,
     anchor = "SW",
     border = "rounded",
-    -- 'editor' and 'win' will default to being centered
     relative = "cursor",
-    -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
     prefer_width = 40,
     width = nil,
-    -- min_width and max_width can be a list of mixed types.
-    -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
-    max_width = { 140, 0.9 },
+    max_width = { 80, 0.9 },
     min_width = { 20, 0.2 },
     buf_options = {},
     win_options = {
-      -- Window transparency (0-100)
       winblend = 15,
       wrap = false,
     },
-    -- Set to `false` to disable
     mappings = {
       n = {
         ["<Esc>"] = "Close",
@@ -69,25 +62,15 @@ require('dressing').setup({
         ["<Down>"] = "HistoryNext",
       },
     },
-
     override = function(conf)
-      -- This is the config that will be passed to nvim_open_win.
-      -- Change values here to customize the layout
       return conf
     end,
-    -- see :help dressing_get_config
     get_config = nil,
   },
   select = {
-    -- Set to false to disable the vim.ui.select implementation
     enabled = true,
-    -- Priority list of preferred vim.select implementations
     backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
-    -- Trim trailing `:` from prompt
     trim_prompt = true,
-    -- Options for telescope selector
-    -- These are passed into the telescope picker directly. Can be used like:
-    -- telescope = require('telescope.themes').get_ivy({...})
     telescope = require('telescope.themes').get_ivy{},
     -- Options for fzf selector
     fzf = {
@@ -123,44 +106,34 @@ require('dressing').setup({
       min_width = 40,
       min_height = 10,
     },
-    -- Options for built-in selector
     builtin = {
       -- These are passed to nvim_open_win
       anchor = "NW",
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "editor",
-
       buf_options = {},
       win_options = {
-        -- Window transparency (0-100)
           winblend = 15,
         },
-        -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-        -- the min_ and max_ options can be a list of mixed types.
-        -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
         width = nil,
         max_width = { 140, 0.8 },
         min_width = { 40, 0.2 },
         height = nil,
         max_height = 0.9,
         min_height = { 10, 0.2 },
-        -- Set to `false` to disable
         mappings = {
           ["<Esc>"] = "Close",
           ["<C-c>"] = "Close",
           ["<CR>"] = "Confirm",
         },
         override = function(conf)
-        -- This is the config that will be passed to nvim_open_win.
         -- Change values here to customize the layout
         return conf
       end,
     },
     -- Used to override format_item. See :help dressing-format
     format_item_override = {},
-    -- see :help dressing_get_config
-    --get_config = nil,
     get_config = function(opts)
       if opts.kind == 'codeaction' then
         return {
@@ -249,7 +222,6 @@ require'nvim-treesitter.configs'.setup {
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
     disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
@@ -294,7 +266,7 @@ require('telescope').setup{
         ["home"]    = "$HOME",
         ["conf"]    = "$HOME/.config",
         ["data"]    = "$HOME/.local/share",
-        ["dev"]     = "$HOME/dev",
+        ["dev"]     = "$HOME/Dev",
       }
     },
     project = {},
